1d6beb9ceca95d216281abc924c34bb17ef62ff8
Sanghyuk Bae <44903787+D3vle0@users.noreply.github.com>
2026-05-18 12:55:34 +0900

    Delete scratch.js

--- scratch.js
+++
@@ -1,7 +0,0 @@
-const React = require('react');
-const firstPChild = "[!WARNING] 전체 페이지 암호화 workflow\n1. Hugo로 렌더링된 최종 HTML을 CLI 입력으로 사용해 암호화 페이로드를 생성한다.";
-const match = firstPChild.match(/^\[!(NOTE|TIP|IMPORTANT|WARNING|CAUTION)\]\s*([^\n]*)/i);
-console.log("match[0]:", match[0]);
-console.log("match[2]:", match[2]);
-let newFirstPChild = firstPChild.slice(match[0].length).replace(/^\s*(?:\r\n|\r|\n)\s*/, "");
-console.log("newFirstPChild:", newFirstPChild);
