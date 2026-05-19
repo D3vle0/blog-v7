03b85ef3e4bd80e66c76435f0f8e643123e1f317
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2026-05-19 22:18:24 +0900

    feat: add allowed dev origin and Firebase storage to image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["192.168.49.136"],
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
@@ -12,6 +13,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "ets-readme-counter.vercel.app",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
       },
     ],
   },
